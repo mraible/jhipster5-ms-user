a8350cdde7afce0a9efb34f260828a2c28329392
Matt Raible <matt.raible@okta.com>
2018-04-12 22:24:56 -0600

    Re-add entities

--- gateway/src/main/webapp/app/entities/entity.module.ts
+++ gateway/src/main/webapp/app/entities/entity.module.ts
@@ -1,12 +1,16 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { GatewayBlogModule as BlogBlogModule } from './blog/blog/blog.module';
+import { GatewayTagModule as BlogTagModule } from './blog/tag/tag.module';
+import { GatewayEntryModule as BlogEntryModule } from './blog/entry/entry.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
         BlogBlogModule,
+        BlogTagModule,
+        BlogEntryModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
